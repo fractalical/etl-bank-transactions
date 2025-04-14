ff4cf931aeb6e929fd01661a5b72f1fb74ecab45
Artur <arturtref@gmail.com>
2025-04-14 15:36:19 +0200

    fixed ods customer dim

--- queries/sta_create_tables.sql
+++ queries/sta_create_tables.sql
@@ -1,45 +1,45 @@
 -- Create Store Dimension Table
 CREATE TABLE STA_store_dim (
-    store_key NVARCHAR(10) PRIMARY KEY,
-    division NVARCHAR(50),
-    district NVARCHAR(50),
-    upazila NVARCHAR(50)
+    store_key VARCHAR(10) PRIMARY KEY,
+    division VARCHAR(50),
+    district VARCHAR(50),
+    upazila VARCHAR(50)
 );
 
 -- Create Item Dimension Table
 CREATE TABLE STA_item_dim (
-    item_key NVARCHAR(10) PRIMARY KEY,
-    item_name NVARCHAR(100),
-    [desc] NVARCHAR(100),
-    unit_price NVARCHAR(50),
-    man_country NVARCHAR(50),
-    supplier NVARCHAR(100),
-    unit NVARCHAR(50)
+    item_key VARCHAR(10) PRIMARY KEY,
+    item_name VARCHAR(100),
+    [desc] VARCHAR(100),
+    unit_price VARCHAR(50),
+    man_country VARCHAR(50),
+    supplier VARCHAR(100),
+    unit VARCHAR(50)
 );
 
 -- Create Customer Dimension Table
 CREATE TABLE STA_customer_dim (
-    coustomer_key NVARCHAR(10) PRIMARY KEY,
-    name NVARCHAR(100),
-    contact_no NVARCHAR(50),
-    nid NVARCHAR(50)
+    coustomer_key VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100),
+    contact_no VARCHAR(50),
+    nid VARCHAR(50)
 );
 
 -- Create Transaction Dimension Table
 CREATE TABLE STA_trans_dim (
-    payment_key NVARCHAR(10) PRIMARY KEY,
-    trans_type NVARCHAR(20),
-    bank_name NVARCHAR(100)
+    payment_key VARCHAR(10) PRIMARY KEY,
+    trans_type VARCHAR(20),
+    bank_name VARCHAR(100)
 );
 
 -- Create Time Dimension Table
 CREATE TABLE STA_time_dim (
-    time_key NVARCHAR(10) PRIMARY KEY,
+    time_key VARCHAR(10) PRIMARY KEY,
     date DATETIME,
 	hour INT,
     day INT,
-	week NVARCHAR(10),
+	week VARCHAR(10),
     month INT,
-    quarter NVARCHAR(10),
+    quarter VARCHAR(10),
     year INT
 );
