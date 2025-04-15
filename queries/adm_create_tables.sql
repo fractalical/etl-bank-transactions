3d5a85760adbbb890194af8238587524158d80f4
Artur <arturtref@gmail.com>
2025-04-15 17:24:22 +0200

    added adm query

---
+++ queries/adm_create_tables.sql
@@ -0,0 +1,45 @@
+-- Create Store Dimension Table
+CREATE TABLE ADM_store_dim (
+    store_key VARCHAR(10) PRIMARY KEY,
+    division VARCHAR(50),
+    district VARCHAR(50),
+    upazila VARCHAR(50)
+);
+
+-- Create Item Dimension Table
+CREATE TABLE ADM_item_dim (
+    item_key VARCHAR(10) PRIMARY KEY,
+    item_name VARCHAR(100),
+    [desc] VARCHAR(100),
+    unit_price VARCHAR(50),
+    man_country VARCHAR(50),
+    supplier VARCHAR(100),
+    unit VARCHAR(50)
+);
+
+-- Create Customer Dimension Table
+CREATE TABLE ADM_customer_dim (
+    coustomer_key VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100),
+    contact_no VARCHAR(50),
+    nid VARCHAR(50)
+);
+
+-- Create Transaction Dimension Table
+CREATE TABLE ADM_trans_dim (
+    payment_key VARCHAR(10) PRIMARY KEY,
+    trans_type VARCHAR(20),
+    bank_name VARCHAR(100)
+);
+
+-- Create Time Dimension Table
+CREATE TABLE ADM_time_dim (
+    time_key VARCHAR(10) PRIMARY KEY,
+    date DATETIME,
+	hour INT,
+    day INT,
+	week VARCHAR(10),
+    month INT,
+    quarter VARCHAR(10),
+    year INT
+);
